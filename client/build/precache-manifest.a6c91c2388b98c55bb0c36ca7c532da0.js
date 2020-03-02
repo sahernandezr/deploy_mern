178ad52f68ccd05d9cf4dbdd4ae13c031768f941
sahernandezr <[E50846950+sahernandezr@users.noreply.github.comgit config --global user.email 50846950+sahernandezr@users.noreply.github.com>
2020-03-01 23:10:04 -0600

    commit after build

---
+++ client/build/precache-manifest.a6c91c2388b98c55bb0c36ca7c532da0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d403980b1d3ca527b1e1d5b1a29872aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8df0c0ea345e7f21b748",
+    "url": "/static/css/main.e4339305.chunk.css"
+  },
+  {
+    "revision": "7de9be04f549284c6ae7",
+    "url": "/static/js/2.c56b5904.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.c56b5904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8df0c0ea345e7f21b748",
+    "url": "/static/js/main.25114e10.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
